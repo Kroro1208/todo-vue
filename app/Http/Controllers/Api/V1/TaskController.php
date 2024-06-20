894e7b6142d1f157fba4395bbd5717f7018e2526
kuroro1208 <yamagata_7580@yahoo.co.jp>
2024-06-20 21:38:44 +0900

    delete()実装した

--- app/Http/Controllers/Api/V1/TaskController.php
+++ app/Http/Controllers/Api/V1/TaskController.php
@@ -54,6 +54,7 @@ class TaskController extends Controller
      */
     public function destroy(Task $task)
     {
-        //
+        $task->delete();
+        return response()->noContent();
     }
 }
